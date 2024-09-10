c3d9940d3aedd026eab2ba59220a77a306e0b3b8
Wtorres893 <wjose.nt@gmail.com>
2024-09-10 03:25:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {titulo : "Lobo Guará",
+    descricao : "O lobo guará é um canídeo de grande porte, endêmico do Cerrado brasileiro. Conhecido por sua pelagem ruiva e patas longas, é um importante predador e dispersor de sementes.",
+    link : "https://pt.wikipedia.org/wiki/Chrysocyon_brachyurus",
+    tags: "canino onívoro" 
+    },
+    {titulo : "Onça-pintada",
+    descricao : "A onça-pintada, o maior felino das Américas, é uma criatura majestosa e poderosa que habita diversas regiões do continente. Com sua pelagem dourada adornada por rosetas negras características, essa espécie é um símbolo da fauna brasileira e de outros países latino-americanos. Apesar de sua força e ferocidade, a onça-pintada desempenha um papel crucial no equilíbrio dos ecossistemas, controlando populações de presas e mantendo a biodiversidade.",
+    link : "https://pt.wikipedia.org/wiki/Panthera_onca",
+    tags: "felídeo pantera"
+    },
+    {titulo : "Harpia",
+    descricao : "A harpia é uma ave de rapina diurna, considerada a águia mais poderosa das Américas. Com garras e bico extremamente fortes, ela se alimenta principalmente de preguiças e macacos.",
+    link : "https://pt.wikipedia.org/wiki/Harpia_harpyja",
+    tags: "rapinante asas"
+    }
+]
